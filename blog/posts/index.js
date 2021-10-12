f82b8a588c1f470c5facba0321c23ea89ff7413e
Carlos Oliveira <carlosoliveiradev@gmail.com>
2021-10-12 16:20:12 -0300

    finish section 2

---
+++ blog/posts/index.js
@@ -0,0 +1,51 @@
+import express from "express";
+import { randomBytes } from "crypto";
+import cors from "cors";
+import axios from "axios";
+
+const app = express();
+
+const posts = {};
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/posts", async (req, res) => {
+  const id = randomBytes(4).toString("hex");
+
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  await axios
+    .post("http://localhost:4005/events", {
+      type: "PostCreated",
+      data: {
+        id,
+        title,
+      },
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+
+  res.status(201).send(posts[id]);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body.type);
+
+  res.send({});
+});
+
+app.listen(4000, () => {
+  console.log("Listening on 4000 PORT");
+});
