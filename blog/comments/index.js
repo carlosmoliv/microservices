1e5c310d9cc4172480d39c37e57c7114edda6f4f
Carlos Oliveira <carlosoliveiradev@gmail.com>
2021-11-30 18:01:45 -0300

    finish some class

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -26,7 +26,7 @@ app.post("/posts/:id/comments", async (req, res) => {
 
   commentsByPostId[req.params.id] = comments;
 
-  await axios.post("http://localhost:4005/events", {
+  await axios.post("http://event-bus-serv:4005/events", {
     type: "CommentCreated",
     data: {
       id: commentId,
@@ -54,7 +54,7 @@ app.post("/events", async (req, res) => {
 
     comment.status = status;
 
-    await axios.post("http://localhost:4005/events", {
+    await axios.post("http://event-bus-serv:4005/events", {
       type: "CommentUpdated",
       data: {
         id,
--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -16,15 +16,15 @@ app.post("/events", (req, res) => {
     console.log(err.message);
   });
 
-  // axios.post("http://localhost:4001/events", event).catch((err) => {
-  //   console.log(err.message);
-  // });
-  // axios.post("http://localhost:4002/events", event).catch((err) => {
-  //   console.log(err.message);
-  // });
-  // axios.post("http://localhost:4003/events", event).catch((err) => {
-  //   console.log(err.message);
-  // });
+  axios.post("http://comments-serv:4001/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://query-serv:4002/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://moderation-serv:4003/events", event).catch((err) => {
+    console.log(err.message);
+  });
   res.send({ status: "OK" });
 });
 
