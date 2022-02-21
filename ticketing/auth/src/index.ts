cefbf0c63797fb663b4190c29f55ae64cf54fa48
Carlos Oliveira <carlosoliveiradev@gmail.com>
2022-02-21 08:03:25 -0300

    finish class 115

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,19 +1,9 @@
 import express from "express";
 import { json } from "body-parser";
 
-import { currentUserRouter } from "./routes/currentUser";
-import { loginRouter } from "./routes/login";
-import { logoutRouter } from "./routes/logout";
-import { registerRouter } from "./routes/register";
-
 const app = express();
 app.use(json());
 
-app.use(currentUserRouter);
-app.use(loginRouter);
-app.use(logoutRouter);
-app.use(registerRouter);
-
-app.listen(4000, () => {
-  console.log("Listening on port 4000!");
+app.listen(3000, () => {
+  console.log("Listening on port 3000!!!!");
 });
